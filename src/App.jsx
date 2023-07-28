6201e1ee7e42bd0df0b826371b832dacc4e00db5
ahmad-ml <ahmad@niftyitsolution.com>
2023-07-28 10:55:22 +0600

    - changed from Switch to Router
    this made previous error fixed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import Income from './pages/Income';
-import Expenses from './pages/Expenses';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Expenses from "./pages/Expenses";
+import Income from "./pages/Income";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <nav>
+        <Home />
         <ul>
-          <li><Link to="/income">Income</Link></li>
-          <li><Link to="/expenses">Expenses</Link></li>
+          <li>
+            <Link to="/income">Income</Link>
+          </li>
+          <li>
+            <Link to="/expenses">Expenses</Link>
+          </li>
         </ul>
       </nav>
-      
-      <Switch>
-        <Route path="/income">
-          <Income />
-        </Route>
-        <Route path="/expenses">
-          <Expenses />
-        </Route>
-      </Switch>
+
+      <Routes>
+        <Route path="/income" element={<Income />} />
+        <Route path="/expenses" element={<Expenses />} />
+      </Routes>
     </div>
   );
 }
